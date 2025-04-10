f51fb57145575a634ef3be764091ddbabefbed82
Sivadheeraj5356 <sivadheerajethamsetti@gmail.com>
2025-04-10 19:12:04 +0530

    ui chnages

--- app/index.tsx
+++ app/index.tsx
@@ -9,119 +9,136 @@ import { Animated } from 'react-native';
 import "../global.css"
 
 export default function HomeScreen() {
-  // Load fonts
   const [fontsLoaded] = useFonts({
     CircularBook: require('../assets/fonts/CircularStd-Book.otf'),
-        CircularLight: require('../assets/fonts/CircularStd-Light.otf'),
-        Cooper : require('../assets/fonts/CooperHewitt-Medium.otf'),
-        NTBrickSans: require('../assets/fonts/NTBrickSans.ttf'),
+    CircularLight: require('../assets/fonts/CircularStd-Light.otf'),
+    Cooper: require('../assets/fonts/CooperHewitt-Medium.otf'),
+    NTBrickSans: require('../assets/fonts/NTBrickSans.ttf'),
   });
 
   const translateX = useRef(new Animated.Value(0)).current;
-  const [currentImage, setCurrentImage] = useState(require('@/assets/images/1.png'));
 
   useEffect(() => {
     const animate = () => {
-      Animated.sequence([
-        Animated.timing(translateX, {
-          toValue: -10, // Move left slightly
-          duration: 4000, // Slow animation duration
-          useNativeDriver: true,
-        }),
-        Animated.timing(translateX, {
-          toValue: 0, // Move back to the original position
-          duration: 4000, // Slow animation duration
-          useNativeDriver: true,
-        }),
-      ]).start(() => {
-        animate(); // Restart the animation
-      });
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(translateX, {
+            toValue: -300,
+            duration: 8000,
+            useNativeDriver: true,
+          }),
+          Animated.timing(translateX, {
+            toValue: 0,
+            duration: 8000,
+            useNativeDriver: true,
+          }),
+        ])
+      ).start();
     };
 
     animate();
   }, [translateX]);
 
-  const [activeTab, setActiveTab] = useState('collections'); // State to track active tab
+  const [activeTab, setActiveTab] = useState('collections');
 
   const Collections = () => (
-    <View style={styles.tabContent} className='h-[532px] w-[100vw] -ml-[32px] bg-[#1b1e24] -mt-[16px]'>
-      <View className='-mt-40'></View>
-        <View style={styles.gridContainer} className='ml-8 mt-20'>
-          <View style={styles.gridItem}>
-            <Image source={require('@/assets/images/liked.png')} />
-            <Image source={require('@/assets/images/rectangle.png')} />
-          </View>
-          <View style={styles.gridItem}>
-            <Image source={require('@/assets/images/saved.png')} />
-            <Image source={require('@/assets/images/rectangle.png')} />
-          </View>
-          <View style={styles.gridItem}>
-            <Image source={require('@/assets/images/files.png')} />
-            <Image source={require('@/assets/images/rectangle.png')} />
-          </View>
+    <View style={styles.tabContent} className='h-[440px] w-[100vw] -ml-[32px] bg-[#1b1e24] -mt-[26px]'>
+      <View style={styles.gridContainer} className='ml-8'>
+        <View style={styles.gridItem}>
+          <Image source={require('@/assets/images/liked.png')} />
+          <Image source={require('@/assets/images/rectangle.png')} />
+        </View>
+        <View style={styles.gridItem}>
+          <Image source={require('@/assets/images/saved.png')} />
+          <Image source={require('@/assets/images/rectangle.png')} />
+        </View>
+        <View style={styles.gridItem}>
+          <Image source={require('@/assets/images/files.png')} />
+          <Image source={require('@/assets/images/rectangle.png')} />
         </View>
       </View>
+    </View>
   );
 
   const ManageTags = () => (
-    <View style={styles.tabContent} className='h-[532px] w-[100vw] -ml-[32px] bg-[#1b1e24] -mt-[16px]'>
-      <View className='-mt-44'>
-      <Text className='text-white text-xl mx-5 mt-16'>our recommendations work best when you let us know these things:</Text>
+    <View style={styles.tabContent} className='h-[440px] w-[100vw] -ml-[32px] bg-[#3d3f44] -mt-[16px]'>
+      <View>
+        <Text className='text-white text-xl mx-5'>our recommendations work best when you let us know these things:</Text>
+      </View>
       <View className='flex flex-row justify-between w-[90%] h-auto mt-7'>
-       <View>
-         <Text className='text-white text-xl ml-5 font-Cooper'>YOUR DIFFICULTY ✨</Text>
-         <Text className='text-white text-base ml-5'>you decide the level of challenge you want!</Text>
-       </View>
-       <View>
-         <Image source={require('@/assets/images/chevron.png')}></Image>
-       </View>
+        <View>
+          <Text className='text-white text-xl ml-5 font-Cooper'>YOUR DIFFICULTY ✨</Text>
+          <Text className='text-white text-base ml-5'>you decide the level of challenge you want!</Text>
+        </View>
+        <View>
+          <Image source={require('@/assets/images/chevron.png')}></Image>
+        </View>
       </View>
-
       <Text className='h-[1px] my-2 w-[95vw] mt-9 bg-white ml-3'></Text>
       <View className='flex flex-row justify-between w-[90%] h-auto mt-10'>
-       <View>
-         <Text className='text-white text-xl ml-5'>INTERESTS YOU LIKE ✨</Text>
-         <Text className='text-white text-base ml-5'>we'll use these to show you cool builds</Text>
-       </View>
-       <View>
-         <Image source={require('@/assets/images/chevron.png')}></Image>
-       </View>
+        <View>
+          <Text className='text-white text-xl ml-5'>INTERESTS YOU LIKE ✨</Text>
+          <Text className='text-white text-base ml-5'>we'll use these to show you cool builds</Text>
+        </View>
+        <View>
+          <Image source={require('@/assets/images/chevron.png')}></Image>
+        </View>
       </View>
-
       <Text className='h-[1px] my-2 w-[95vw] mt-9 bg-white ml-3'></Text>
       <View className='flex flex-row justify-between w-[90%] h-auto mt-10'>
-       <View>
-         <Text className='text-white text-xl ml-5'>TOOLS USED ✨</Text>
-         <Text className='text-white text-base ml-5'>We’ll suggest better using these picks.</Text>
-       </View>
-       <View>
-         <Image source={require('@/assets/images/chevron.png')}></Image>
-       </View>
+        <View>
+          <Text className='text-white text-xl ml-5'>TOOLS USED ✨</Text>
+          <Text className='text-white text-base ml-5'>We’ll suggest better using these picks.</Text>
+        </View>
+        <View>
+          <Image source={require('@/assets/images/chevron.png')}></Image>
+        </View>
       </View>
-
       <Text className='h-[1px] my-2 mt-9 w-[95vw] bg-white ml-3'></Text>
-      
-      </View>
-     
-
     </View>
   );
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={
         <LinearGradient
-          colors={['#0b002f', '#23044A']} // Define the gradient colors
-          start={{ x: 0.5, y: 0 }} // Start at the top center
-          end={{ x: 0.5, y: 1 }}  
-          style={StyleSheet.absoluteFillObject} // Ensure it fills the header
+          colors={['#0b002f', '#23044A']}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 1 }}
+          style={StyleSheet.absoluteFillObject}
         />
       }
       headerImage={
         <View style={styles.headerContainer}>
-          <Animated.Image
-            source={currentImage}
-            style={[styles.headerImage, { transform: [{ translateX }] }]}
-          />
+          <View style={styles.gradientImageContainer}>
+            <Animated.Image
+              source={require('@/assets/images/1.png')}
+              style={[styles.headerImage, { transform: [{ translateX }] }]}
+            />
+            {/* <Animated.Image
+              source={require('@/assets/images/2.png')}
+              style={[styles.headerImage, { transform: [{ translateX:300}] }]}
+            /> */}
+            <LinearGradient
+              colors={['rgba(0,0,0,0.5)', 'rgba(0,0,0,0)']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={StyleSheet.absoluteFillObject}
+            />
+          </View>
+          {/* <View style={styles.gradientImageContainer}>
+            
+            <Animated.Image
+              source={require('@/assets/images/2.png')}
+              style={[styles.headerImage, { transform: [{ translateX }] }]}
+            />
+            <LinearGradient
+              colors={['rgba(0,0,0,0.5)', 'rgba(0,0,0,0)']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={StyleSheet.absoluteFillObject}
+            />
+          </View> */}
           <View style={styles.headerTextContainer}>
             <View style={[styles.flexContainer]}>
               <View>
@@ -130,86 +147,86 @@ export default function HomeScreen() {
               <View style={[styles.centerContainer]}>
                 <Image source={require('@/assets/images/Vector.png')} />
                 <Image source={require('@/assets/images/Cog.png')} />
-              </View> 
+              </View>
             </View>
-            
             <View style={styles.textContainer}>
               <View style={styles.row}>
                 <Text style={styles.textWhite}>@theo_from_hsr</Text>
                 <Image style={styles.badgeImage} source={require('@/assets/images/badge.png')} />
               </View>
-
               <View style={styles.row}>
                 <Text style={styles.editProfileText}>EDIT PROFILE</Text>
                 <Image style={styles.editProfileIcon} source={require('@/assets/images/Pen.png')} />
-              </View>         
+              </View>
             </View>
             <View>
               <View className='flex flex-row justify-start h-auto w-[100%] gap-2 ml-[13px] mt-2'>
                 <Image source={require('@/assets/images/IN.png')} className='-mt-44 w-8' />
                 <Text className='text-white text-xl -mt-[160px]'>INDIA</Text>
               </View>
-                  
               <View className='flex w-[100%] h-auto justify-center items-center mr-[14px] mt-4'>
-                 <Text className='mx-5 text-white text-xl -mt-60'>18 y/o with high ambitions. want to build cool stuff!</Text>
-              </View>   
-     
-          <View className='-mt-20 ml-6'>
-            <View className='flex flex-row justify-start w-[90%] gap-2'>
-                <View>
-                  <Image source={require('@/assets/images/user.png')} className='w-6'></Image>
+                <Text className='mx-5 text-white text-xl -mt-60'>18 y/o with high ambitions. want to build cool stuff!</Text>
+              </View>
+              <View className='-mt-20 ml-6'>
+                <View className='flex flex-row justify-start w-[90%] gap-2'>
+                  <View>
+                    <Image source={require('@/assets/images/user.png')} className='w-6'></Image>
+                  </View>
+                  <View>
+                    <Text className='text-white text-2xl -mt-1 '>2</Text>
+                  </View>
                 </View>
                 <View>
-                   <Text className='text-white text-2xl -mt-1 '>2</Text>
+                  <Text className='text-white text-xl'>FOLLOWING</Text>
                 </View>
               </View>
-              <View>
-                <Text className='text-white text-xl'>FOLLOWING</Text>
-              </View>
-          </View>
-             
-
             </View>
-
           </View>
         </View>
       }
     >
       <View className='w-[100vw] -ml-[32px] h-[65px] bg-black -mt-[32px]'>
-<View className='flex flex-row justify-between w-[100%] h-auto'>
-        <TouchableOpacity
-          style={[styles.tabButton, activeTab === 'collections' && styles.activeTab]}
-          onPress={() => setActiveTab('collections')}
-        >
-          <View className='flex flex-row justify-center items-center gap-2 pt-4 pl-4'>
-         { activeTab === 'collections'? <Image source={require('@/assets/images/collections.png')}></Image> : <Image source={require('@/assets/images/collections0.png')}></Image>} 
-          <Text style={[styles.tabButtonText, activeTab === 'collections' && styles.activeTabText]} className='text-2xl '>
-            COLLECTIONS
-          </Text>
-          </View>
-          {activeTab === 'collections' && <Text className='h-[1px] my-1 bg-[#08b078]'></Text>} 
-
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.tabButton, activeTab === 'manageTags' && styles.activeTab]}
-          onPress={() => setActiveTab('manageTags')}
-        >
-         <View className='flex flex-row justify-center items-center gap-2 pt-4 pr-4'>
-        {activeTab === 'manageTags' ?<Image source={require('@/assets/images/managetabs1.png')}></Image> : <Image source={require('@/assets/images/managetabs.png')}></Image> }     
-          <Text style={[styles.tabButtonText, activeTab === 'manageTags' && styles.activeTabText]} className='text-2xl '>
-            MANAGE TAGS
-          </Text>
-          </View>
-         {activeTab === 'manageTags' && <Text className='h-[1px] my-1 bg-[#08b078]'></Text>} 
-        </TouchableOpacity>
+        <View className='flex flex-row justify-between w-[100%] h-auto'>
+          <TouchableOpacity
+            style={[styles.tabButton, activeTab === 'collections' && styles.activeTab]}
+            onPress={() => setActiveTab('collections')}
+          >
+            <View className='flex flex-row justify-center items-center gap-2 pt-4 pl-4'>
+              {activeTab === 'collections' ? (
+                <Image source={require('@/assets/images/collections.png')}></Image>
+              ) : (
+                <Image source={require('@/assets/images/collections0.png')}></Image>
+              )}
+              <Text style={[styles.tabButtonText, activeTab === 'collections' && styles.activeTabText]} className='text-2xl '>
+                COLLECTIONS
+              </Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              {activeTab === 'collections' && <Text className='h-[1px] my-1 bg-[#08b078]'></Text>}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.tabButton, activeTab === 'manageTags' && styles.activeTab]}
+            onPress={() => setActiveTab('manageTags')}
+          >
+            <View className='flex flex-row justify-center items-center gap-2 pt-4 pr-4'>
+              {activeTab === 'manageTags' ? (
+                <Image source={require('@/assets/images/managetabs1.png')}></Image>
+              ) : (
+                <Image source={require('@/assets/images/managetabs.png')}></Image>
+              )}
+              <Text style={[styles.tabButtonText, activeTab === 'manageTags' && styles.activeTabText]} className='text-2xl '>
+                MANAGE TAGS
+              </Text>
+            </View>
+            {activeTab === 'manageTags' && <Text className='h-[1px] my-1 bg-[#08b078]'></Text>}
+          </TouchableOpacity>
+        </View>
       </View>
+      <View className='overflow-y-hidden'>{activeTab === 'collections' ? <Collections /> : <ManageTags />}</View>
+      <View style={{ backgroundColor: 'black', flex: 1 }}>
+        <Footer />
       </View>
-
-      {activeTab === 'collections' ? <Collections /> : <ManageTags />}
-
-      <View style={{ backgroundColor: 'black', flex: 1 }} className='relative'>
-  <Footer />
-     </View>
     </ParallaxScrollView>
   );
 }
@@ -217,15 +234,21 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   headerContainer: {
     flex: 1,
-    position: 'relative', 
-    overflow: 'hidden', // Ensure the image doesn't overflow the container
+    position: 'relative',
+    overflow: 'hidden',
   },
   headerImage: {
+    width: '180%',
+    height: '100%',
+    position: 'absolute',
+    resizeMode: 'cover',
+    zIndex: 1,
+    marginRight: 40,
+  },
+  gradientImageContainer: {
+    position: 'relative',
     width: '100%',
     height: '100%',
-    position: 'absolute', 
-    resizeMode: 'cover', // Ensure the image covers the container
-    zIndex : 1,
   },
   headerTextContainer: {
     flex: 1,
@@ -237,9 +260,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    zIndex:2,
+    zIndex: 2,
     marginTop: 50,
-   
   },
   headerTitle: {
     fontSize: 28,
@@ -295,7 +317,7 @@ const styles = StyleSheet.create({
   textWhite: {
     color: '#FFFFFF',
     fontSize: 20,
-    marginRight: 8, // Add spacing between text and badge
+    marginRight: 8,
   },
   badgeImage: {
     width: 20,
@@ -303,31 +325,19 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
-    alignItems: 'center', // Align items vertically in the row
+    alignItems: 'center',
   },
   editProfileText: {
     color: '#CCCCCC',
     fontSize: 16,
-    marginRight: 8, // Add spacing between text and icon
+    marginRight: 8,
   },
   editProfileIcon: {
     width: 16,
     height: 16,
   },
-  tabContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 32,
-    backgroundColor: 'black',
-    width: '100%',
-    alignSelf: 'stretch',
-  },
-  tabButton: {
-    
-  },
-  activeTab: {
-    // backgroundColor: '#6200EE',
-  },
+  tabButton: {},
+  activeTab: {},
   tabButtonText: {
     color: '#CCCCCC',
     fontSize: 16,
@@ -341,23 +351,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black',
-
-
   },
   tabText: {
     color: '#FFFFFF',
     fontSize: 18,
   },
   gridContainer: {
-    flexDirection: 'row', // Arrange items in a row
-    flexWrap: 'wrap', // Allow wrapping to the next row
-    justifyContent: 'space-between', // Add space between items
-    marginTop: 10, // Equivalent to `mt-10`
-    width: '100%', // Equivalent to `w-[100vw]`
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    width: '100%',
   },
   gridItem: {
-    width: '48%', // Two items per row (48% to account for spacing)
-    marginBottom: 10, // Equivalent to `gap-5`
+    width: '48%',
+    marginBottom: 10,
   },
   container: {
     flex: 1,
@@ -366,17 +374,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#101216',
   },
   boldText: {
-    fontFamily: 'CircularBold', // Use the loaded font
+    fontFamily: 'CircularBold',
     fontSize: 20,
     color: '#FFFFFF',
   },
   mediumText: {
-    fontFamily: 'CircularMedium', // Use the loaded font
+    fontFamily: 'CircularMedium',
     fontSize: 18,
     color: '#CCCCCC',
   },
   regularText: {
-    fontFamily: 'CircularRegular', // Use the loaded font
+    fontFamily: 'CircularRegular',
     fontSize: 16,
     color: '#AAAAAA',
   },
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,68 +2,76 @@ import { Image, StyleSheet, View, Text } from 'react-native';
 import { useFonts } from 'expo-font';
 
 const Footer = () => {
-    const [fontsLoaded] = useFonts({
-        CircularBook: require('../assets/fonts/CircularStd-Book.otf'),
-        CircularLight: require('../assets/fonts/CircularStd-Light.otf'),
-        Cooper : require('../assets/fonts/CooperHewitt-Medium.otf'),
-        NTBrickSans: require('../assets/fonts/NTBrickSans.ttf'),
-      });
+  const [fontsLoaded] = useFonts({
+    CircularBook: require('../assets/fonts/CircularStd-Book.otf'),
+    CircularLight: require('../assets/fonts/CircularStd-Light.otf'),
+    Cooper: require('../assets/fonts/CooperHewitt-Medium.otf'),
+    NTBrickSans: require('../assets/fonts/NTBrickSans.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null; 
+  }
+
   return (
-    <View style={styles.footerContainer} className='w-[100vw] -ml-[32px] -mt-[40px] relative'>
-        {/* <Text className='h-[1px] w-[100%] my-1 -mt-10 bg-white'></Text> */}
+    <View style={styles.footerContainer} className='w-[100vw]'>
+      <View>
       <Image
         source={require('@/assets/images/footerimg.png')}
         style={styles.backgroundImage}
+       
       />
+      </View>
 
       <Image
         source={require('@/assets/images/glitch.png')}
         style={styles.glitchImage}
-        className='absolute top-[-50px] left-[-50px] w-[200px] h-[200px]'
+        className='-ml-[20px] right-5'
       />
 
-      <Text style={styles.joinedText} className='font-CircularRegular'>JOINED 2242 DAYS AGO</Text>
+      <Text style={styles.joinedText}>JOINED 2242 DAYS AGO</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   footerContainer: {
-    position: 'relative', // Allows positioning of child elements
-    backgroundColor: 'black', // Set background color to black
-    height: 150, // Set height of the footer
-    justifyContent: 'center', // Center content vertically
-    alignItems: 'center', // Center content horizontally
-    overflow: 'hidden', // Prevent overflow
-    margin: 0, // Remove any margins
-    padding: 0, // Remove any padding
+    position: 'relative',
+    backgroundColor: 'black',
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+    margin: 0,
+    padding: 0,
     marginVertical: -32,
+    marginLeft: -32,
     alignSelf: 'stretch',
   },
   backgroundImage: {
-    width: '100%', // Full width of the container
-    height: '100%', // Full height of the container
-    resizeMode: 'cover', // Ensures the image covers the entire container
-    position: 'absolute', // Makes it the background
-    zIndex: 1, // Ensures it stays behind other elements
-    top: 0, // Align to the top of the container
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%', 
+    resizeMode: 'cover',
+    zIndex: 1,
   },
   glitchImage: {
-    position: 'absolute', // Position it on top of the background
-    top: '50%', // Center vertically
-    left: '50%', // Center horizontally
-    transform: [{ translateX: -50 }, { translateY: -50 }], // Adjust for centering
-    width: 100, // Adjust size as needed
-    height: 100, // Adjust size as needed
-    resizeMode: 'contain', // Maintain aspect ratio
-    zIndex: 2, // Ensure it stays above the background
+    position: 'absolute',
+    top: '50%',
+    right: 20,
+    transform: [{ translateX: -100 }, { translateY: -60 }],
+    width: 160,
+    height: 160,
+    resizeMode: 'contain',
+    zIndex: 2, 
   },
   joinedText: {
-    position: 'absolute', // Position it relative to the footer
-    bottom: 10, // Place it at the bottom of the footer
-    color: 'white', // Set text color to white
-    fontSize: 16, // Adjust font size
-    zIndex: 3, // Ensure it stays above the background
+    position: 'absolute',
+    bottom: 10,
+    color: 'white',
+    fontSize: 13,
+    zIndex: 3, 
   },
   container: {
     flex: 1,
@@ -77,12 +85,12 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
   },
   mediumText: {
-    fontFamily: 'CircularMedium', // Use the loaded font
+    fontFamily: 'CircularMedium',
     fontSize: 18,
     color: '#CCCCCC',
   },
   regularText: {
-    fontFamily: 'CircularRegular', // Use the loaded font
+    fontFamily: 'CircularRegular',
     fontSize: 16,
     color: '#AAAAAA',
   },
